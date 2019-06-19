12db24a655b0f5438b9365f80873a1a370ec8ae6
Rowan Clarke <36474636+rowanclarke@users.noreply.github.com>
2019-06-19 16:18:56 +0100

    Update main.c
    Can now support any dimensions by using 'data' as int*

--- src/main.c
+++ src/main.c
@@ -7,9 +7,12 @@ LRESULT CALLBACK WndProc (HWND hWnd, UINT message,
 WPARAM wParam, LPARAM lParam);
 void EnableOpenGL (HWND hWnd, HDC *hDC, HGLRC *hRC);
 void DisableOpenGL (HWND hWnd, HDC hDC, HGLRC hRC);
-const int W = 399;
-const int H = 399;
-const int RND = 3;
+int W;
+int H;
+const int RND = 3;					// -a-b-g-r
+const unsigned int BACK = 0;	 	// 00000000
+const unsigned int FORE = 16777215; // 00FFFFFF
+int zoom = 1;
 
 int WINAPI WinMain (HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int iCmdShow) {
     WNDCLASS wc;
@@ -20,10 +23,13 @@ int WINAPI WinMain (HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLin
     BOOL bQuit = FALSE;
     BOOL* screen;
     BOOL* buffer;
+    int* data;
     float theta = 0.0f;
+    W = GetSystemMetrics(SM_CXSCREEN);
+	H = GetSystemMetrics(SM_CYSCREEN);
     screen = calloc(sizeof(BOOL)*W*H, 1);
     buffer = calloc(sizeof(BOOL)*W*H, 1);
-    
+    data =  malloc(sizeof(int)*W*H*3);
     wc.style = CS_OWNDC;
     wc.lpfnWndProc = WndProc;
     wc.cbClsExtra = 0;
@@ -39,14 +45,14 @@ int WINAPI WinMain (HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLin
     
     hWnd = CreateWindow (
       "GoL", "Conway's Game of Life", 
-      WS_CAPTION | WS_POPUPWINDOW | WS_VISIBLE,
+      WS_POPUPWINDOW | WS_VISIBLE | WS_OVERLAPPED,
       0, 0, W, H,
       NULL, NULL, hInstance, NULL);
       
     EnableOpenGL (hWnd, &hDC, &hRC);
 
 	int i, j;
-
+	
 	srand(time(NULL));
 	
 	for (i = 0; i < H; i++) {
@@ -56,6 +62,7 @@ int WINAPI WinMain (HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLin
 	}
 
     while (!bQuit) {
+    	
         if (PeekMessage (&msg, NULL, 0, 0, PM_REMOVE)) {
             if (msg.message == WM_QUIT) bQuit = TRUE;
             else {
@@ -67,20 +74,21 @@ int WINAPI WinMain (HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLin
         	glClearColor (0.0f, 0.0f, 0.0f, 0.0f);
             glClear (GL_COLOR_BUFFER_BIT);
             
-		    int data[H][W][3]; 
+		    
 		    for (i = 0; i < H; i++) {
 		        for (j = 0; j < W; j++) {
-		        	unsigned int col = 0;
-		        	if (screen[i*W+j]) col = 4278190080;
-		            data[i][j][0] = col;
-		            data[i][j][1] = col;
-		            data[i][j][2] = col;
+		        	int col = BACK;
+		        	if (screen[i*W+j]) col = FORE;
+		            data[(i*W+j)*3] = ((col >> 0) & 0x000000ff) * 16843008;
+					data[(i*W+j)*3+1] = ((col >> 8) & 0x000000ff) * 16843008;
+					data[(i*W+j)*3+2] = ((col >> 16) & 0x000000ff) * 16843008;
 		        }
 		    }
-		
+			
 		    glDrawPixels(W, H, GL_RGB, GL_UNSIGNED_INT, data);
+            glPixelZoom(zoom, zoom);
             
-	    	int i, j, k, l;
+	    	int k, l;
 	    	for (i = 0; i < H; i++) {
 				for (j = 0; j < W; j++) {
 					int neighbours = 0;
@@ -124,13 +132,12 @@ LRESULT CALLBACK WndProc (HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
     switch (message) {
     case WM_CREATE:
         return 0;
+    case WM_DESTROY:
+        return 0;
     case WM_CLOSE:
         PostQuitMessage (0);
         return 0;
-
-    case WM_DESTROY:
-        return 0;
-
+        
     case WM_KEYDOWN:
         switch (wParam)
         {
@@ -139,6 +146,9 @@ LRESULT CALLBACK WndProc (HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
             return 0;
         }
         return 0;
+	case WM_MOUSEWHEEL:
+		zoom += GET_WHEEL_DELTA_WPARAM(wParam) / 120;
+		return 0;
 
     default:
         return DefWindowProc (hWnd, message, wParam, lParam);
